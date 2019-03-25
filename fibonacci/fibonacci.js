7c3c62e0039e780bd12373f86b25549997ab539d
Nick Smith <smithne@uw.edu>
2019-03-24 17:24:21 -0700

    finish through javascript fundabements 5 (except pig latin)

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(sequence) {
+    if (sequence < 1) {
+        return "OOPS";
+    } else if (sequence == 1) {
+        return 1;
+    }
+    
+    let first = 0;
+    let second = 1;
+    let counter = 1; 
 
+    while (counter < sequence) {
+        let holder = first;
+        first = second;
+        second = first + holder;
+        counter++;
+    }
+
+    return second;
 }
 
 module.exports = fibonacci
+
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function() {
+const palindromes = function(inputString) {
+    reversed = "";
 
+    inputString = inputString.toLowerCase().replace(/[^A-Za-z]/g, "");
+    for (let i = inputString.length - 1; i >= 0; i--) {
+        reversed += inputString.charAt(i);
+    }
+    console.log(inputString + ' ' + reversed);
+    return reversed == inputString;
 }
 
 module.exports = palindromes
